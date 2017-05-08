0ce00fb34114ff723c10acdbce775c0d814c2638
Katherina Vovk <vovk.katya@gmail.com>
2017-05-08 17:32:51 +0300

    add arrows for slideshow

--- index.php
+++ index.php
@@ -13,19 +13,81 @@
         </div>
       
         <div class="content-box">
-            <h2>Main page</h2>
-            <div class="slideshow">
+            <div id="slideshow">
+                <div class="img-content">
+                    <div class="prev"></div> 
+                    <img src="images/cat.jpg" width="1170px" id="imgClickChange" onclick="changeImage()">
+                   <div class="next"></div> 
+                </div>
+               
+                <script language="javascript">
+                    var imgs = ["images/kitty.jpg", "images/kotenok.jpg"];
+                    function changeImage(dir) {
+                        var imgElement = document.getElementById('imgClickAndChange'),
+                      
+                
+                    }
+                </script>
             </div>
         </div>
    
 
 
-    <center class="col-md-6">
-       
+    <center class="col-md-8">
+        <div class="comment" style="display: none">
+            <p>Leave a comment:</p>
+            
+            <input type="text" name="text">
+            
+            <button class = "btn btn-primary btn-md btn-submit" type="button" name="comment" id="register" data-tooltip="status-close">Post comment</button>
+        </div>
     </center>
 
   </div>
 </div>
+    <style>
+        /* slideshow*/
+#slideshow{
+    width: 1170px;
+  /*  border: 1px solid red;*/
+    position:relative;
+    margin-top: 50px;
+}
 
+
+.img-content{
+    max-height: 560px;
+   
+}
+.img-content .prev:before{
+    content: "<<";
+    display: block;
+  /*  border: 1px solid green;*/
+    float: left;
+    position:absolute;
+    left:0;
+    top: 270px;
+    width: 30px;
+    height: 30px;
+    padding: 5px;
+}
+.img-content .prev:before:hover, .img-content .next:before:hover{
+    color:red;
+
+}
+.img-content .next:before{
+    content: ">>";
+    display: block;
+ /*   border: 1px solid green;*/
+    float: left;
+    position:absolute;
+    right:0;
+    top: 270px;
+    width: 30px;
+    height: 30px;
+    padding: 5px;
+}
+.img-content span{}
+    </style>
 </body>
 </html>
