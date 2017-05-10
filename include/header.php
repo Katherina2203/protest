a7e9dddd82d598ef8ed507ff19083401496237f1
Katherina Vovk <vovk.katya@gmail.com>
2017-05-10 10:59:50 +0300

    slideshow edited

--- include/header.php
+++ include/header.php
@@ -4,8 +4,12 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <link rel="stylesheet" href = "css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+        <link href="./css/style.js" rel="stylesheet">
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.js"></script>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
         	<link rel="stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" >
      <!--   <script src="js/jquery.maskedinput-1.3.min.js"></script>-->
+                        
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/registration.js"></script>
 	<script src="js/refreshform.js"></script>
--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php include "include/header.php";?>
 	<title>Register</title>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<script type="text/javascript" src="js/registration.js"></script>
-	<script src="js/mytest.js" async></script>
-    
+
+        <script src="/protest/js/slideshow.js" defer></script>
 </head>
 <body>
 <div class="wrapper">
@@ -15,49 +13,107 @@
         <div class="content-box">
             <div id="slideshow">
                 <div class="img-content">
-                    <div class="prev" onclick="plusSlides(-1)"><a href=""><<</a></div> 
-                    <div class="img">
-                        <img src="images/cat.jpg" width="1170px" id="imgClickChange" onclick="currentSlide()">
-                    </div>
-                   <div class="next" onclick="plusSlides(1)"><a href="">>></a></div> 
-                   
-                   <div style="text-align:center">
+                    <a class="prev" onclick="plusSlides(-1)"><<</a> 
+                        <div class="img">
+                            <img src="images/cat.jpg" width="1170px" height="672px" onclick="currentSlide()">
+                        </div>
+                        <div class="img">
+                            <img src="images/kitty.jpg" width="1170px" height="672px" onclick="currentSlide()">
+                        </div>
+                        <div class="img">
+                            <img src="images/kotenok.jpg" width="1170px" height="672px" onclick="currentSlide()">
+                        </div>
+                        <div class="img">
+                            <img src="images/kotyata_s_rynka.jpg" width="1170px" height="672px" onclick="currentSlide()">
+                        </div>
+                    <a class="next" onclick="plusSlides(1)">>></a> 
+                    <div style="text-align:center">
                         <span class="dot" onclick="currentSlide(1)"></span> 
                         <span class="dot" onclick="currentSlide(2)"></span> 
-                        <span class="dot" onclick="currentSlide(3)"></span> 
+                        <span class="dot" onclick="currentSlide(3)"></span>
+                        <span class="dot" onclick="currentSlide(4)"></span> 
                     </div>
                 </div>
-               
-                <script language="javascript">
-                   var slideIndex = 1;
-                   showSlides(slideIndex);
-                   
-
-                    function showSlides(n) {
-                        var slides = document.getElementByClassName('.img'),
-                            dots = document.getElementByClassName('.dot');
-                      
-                        for(var i=0; i<slides.length; i++){
-                            slides[i].style.display = "none";
-                        }
-                        for(var i=0; i<dots.length; i++){
-                            dots[i].className = dots[i].className.replace("active", "");
-                        }
                 
+              
+            </div>
+        </div>
+        <script lang="javascript">
+            var slideIndex = 1;
+                    showSlides(slideIndex);
+                    
+                    function showSlides(n) {
+                        var slides = document.getElementsByClassName("img"),
+                            dots = document.getElementsByClassName("dot");
+                      
+                      if (n > slides.length) {slideIndex = 1} 
+                      if (n < 1) {slideIndex = slides.length}
+                        for (var i = 0; i < slides.length; i++) {
+                            slides[i].style.display = "none"; 
+                        }
+                        for (var i = 0; i < dots.length; i++) {
+                            dots[i].className = dots[i].className.replace(" active", "");
+                        }
+                        slides[slideIndex-1].style.display = "block"; 
+                        dots[slideIndex-1].className += " active";
                     }
+                    
                     function currentSlide(){
                         showSlides(slideIndex = n);
                     }
             
                     function plusSlides(n){
                         showSlides(slideIndex += n);
-                        
                     }
-                </script>
-            </div>
-        </div>
-   
+        </script>
+        <style>
+                    /*slideshow */
+        #slideshow{
+            width: 1170px;
+            position:relative;
+            margin-top: 50px;
+        }
 
+        .img{display:none;}
+
+        .img-content .prev, .img-content .next{
+            display: block;
+            background: green;
+            color: #fff;
+            opacity: 0.5;
+            float: left;
+            position:absolute;
+            background-position: 0 0;
+            padding: 5px;
+        }
+
+        .img-content .prev{
+            left:0;
+            top: 270px;
+        }
+        .img-content .prev:hover, .img-content .next:hover{
+            text-decoration: none;
+            opacity: 1;
+        }
+        .img-content .next{
+            right:0;
+            top: 270px;
+        }
+
+        .dot {
+          cursor:pointer;
+          height: 13px;
+          width: 13px;
+          margin: 0 2px;
+          background-color: #bbb;
+          border-radius: 50%;
+          display: inline-block;
+          transition: background-color 0.6s ease;
+        }
+        .active, .dot:hover {
+          background-color: #717171;
+        }   
+        </style>
 
     <center class="col-md-8">
         <div class="comment" style="display: none">
@@ -71,58 +127,6 @@
 
   </div>
 </div>
-    <style>
-        /* slideshow*/
-#slideshow{
-    width: 1170px;
-  /*  border: 1px solid red;*/
-    position:relative;
-    margin-top: 50px;
-}
-
-
-.img-content{
-    max-height: 560px;
    
-}
-
-.img-content .prev a, .img-content .next a{
-    display: block;
-    background: green;
-    color: #fff;
-    opacity: 0.5;
-    float: left;
-    position:absolute;
-    background-position: 0 0;
-    padding: 5px;
-}
-    
-.img-content .prev a{
-    left:0;
-    top: 270px;
-}
-.img-content .prev a:hover, .img-content .next a:hover{
-    text-decoration: none;
-    opacity: 1;
-}
-.img-content .next a{
-    right:0;
-    top: 270px;
-}
-
-.dot {
-  cursor:pointer;
-  height: 13px;
-  width: 13px;
-  margin: 0 2px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  transition: background-color 0.6s ease;
-}
-.active, .dot:hover {
-  background-color: #717171;
-}
-    </style>
 </body>
 </html>
